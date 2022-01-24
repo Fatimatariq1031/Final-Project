af5837562acb0285d786ea4f254012236db78ec4
Fatimatariq1031 <amnakhan1031@gmail.com>
2022-01-24 23:52:39 +0500

    APP BAR

--- app/src/main/java/com/example/myapplication/Adapters/MainAdapter.java
+++ app/src/main/java/com/example/myapplication/Adapters/MainAdapter.java
@@ -1,2 +1,61 @@
-package com.example.myapplication.Adapters;public class MainAdapter {
+package com.example.myapplication.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.MainActivity;
+import com.example.myapplication.Models.MainModel;
+import com.example.myapplication.R;
+
+import java.util.ArrayList;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.viewholder> {
+    @NonNull
+    ArrayList <MainModel> list;
+    Context context;
+
+    public MainAdapter(ArrayList<MainModel> list, MainActivity mainActivity) {
+    }
+
+
+    @Override
+    public viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.sample_mainpharmacy,parent,false);
+        return new viewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewholder holder, int position) {
+final MainModel model=list.get(position);
+holder.medimage.setImageResource(model.getImage());
+holder.price.setText(model.getPrice());
+holder.description.setText(model.getDescription());
+holder.mainName.setText(model.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class viewholder extends RecyclerView.ViewHolder {
+        ImageView medimage;
+        TextView mainName, price,description;
+        public viewholder(@NonNull View itemView) {
+            super(itemView);
+            medimage=itemView.findViewById(R.id.medimage);
+            mainName=itemView.findViewById(R.id.name);
+            price=itemView.findViewById(R.id.price);
+            description=itemView.findViewById(R.id.desc);
+
+        }
+    }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.myapplication.Adapters.MainAdapter;
+import com.example.myapplication.Models.MainModel;
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ArrayList<MainModel> list= new ArrayList<>();
+        list.add(new MainModel(R.drawable.ic_launcher_background, "Med1", "5", "This is medicine"));
+
+        list.add(new MainModel(R.drawable.ic_launcher_background, "Med1", "5", "This is medicine"));
+        list.add(new MainModel(R.drawable.ic_launcher_background, "Med1", "5", "This is medicine"));
+        list.add(new MainModel(R.drawable.ic_launcher_background, "Med1", "5", "This is medicine"));
+        list.add(new MainModel(R.drawable.ic_launcher_background, "Med1", "5", "This is medicine"));
+        MainAdapter adapter = new MainAdapter(list, this);
+
+        binding.recyclerview.setAdapter(adapter);
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        binding.recyclerview.setLayoutManager(layoutManager);
+
+
     }
 }
