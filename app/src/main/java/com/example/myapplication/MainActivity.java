3d7dfc4083c7b77a2dfd3ab66843829cb321ad29
bsef18a544 <91981689+FatimaShah11@users.noreply.github.com>
2022-02-02 14:58:26 +0500

    medicine array in RecyclerView

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,39 +1,79 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.myapplication.Adapters.MainAdapter;
 import com.example.myapplication.Models.MainModel;
 import com.example.myapplication.databinding.ActivityMainBinding;
+import com.example.myapplication.databinding.ActivityMainStartBinding;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+
     ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        ArrayList<MainModel> list= new ArrayList<>();
-        list.add(new MainModel(R.drawable.ic_launcher_background, "Med1", "5", "This is medicine"));
 
-        list.add(new MainModel(R.drawable.ic_launcher_background, "Med1", "5", "This is medicine"));
-        list.add(new MainModel(R.drawable.ic_launcher_background, "Med1", "5", "This is medicine"));
-        list.add(new MainModel(R.drawable.ic_launcher_background, "Med1", "5", "This is medicine"));
-        list.add(new MainModel(R.drawable.ic_launcher_background, "Med1", "5", "This is medicine"));
-        MainAdapter adapter = new MainAdapter(list, this);
+        ArrayList<MainModel> list = new ArrayList<>();
+        list.add(new MainModel(R.drawable.cough, "Robitussin", "5", "This is medicine for treating cough"));
+        list.add(new MainModel(R.drawable.rizek, "Rizek", "4", "This is medicine for treating covid symptoms"));
+        list.add(new MainModel(R.drawable.fever, "Paracetamol", "2", "This is medicine for treating fever"));
+        MainAdapter adapter;
+        adapter = new MainAdapter(list, this);
 
         binding.recyclerview.setAdapter(adapter);
-        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         binding.recyclerview.setLayoutManager(layoutManager);
 
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.orders: {
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+
+                startActivity(intent);
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
 }
+
+
+
